ee8f5cb0907a819533ac5c064150893336e5c620
IT22267122 <it22267122@my.sliit.lk>
2025-05-10 20:29:27 +0530

    Update EditCommentModal.jsx

--- client/src/Components/Comment/EditCommentModal.jsx
+++ client/src/Components/Comment/EditCommentModal.jsx
@@ -12,7 +12,42 @@ import {
 
   function EditCommentModal({ isOpen, onOpen, onClose,handleProfileImageChange }) {
   
-    
+    return (
+      <>
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader textAlign={"center"}>Modal Title</ModalHeader>
+  
+            <ModalBody>
+              <div className="flex flex-col items-center">
+                <label
+                  for="profileImage"
+                  className="font-bold py-3 text-blue-600 text-center cursor-pointer text-xs w-full"
+                >
+                 Delete
+                </label>
+  
+                <input onChange={handleProfileImageChange} type="file" id="profileImage" name="profileImage" />
+              </div>
+  
+              <hr />
+  
+              <p className="font-bold py-3 text-red-600 text-center">
+                Edit
+              </p>
+              <hr />
+              <p className=" py-3 text-center" onClick={onClose}>
+                Cancel
+              </p>
+            </ModalBody>
+            {/* <ModalFooter>
+                <Button onClick={onClose}>Close</Button>
+              </ModalFooter> */}
+          </ModalContent>
+        </Modal>
+      </>
+    );
   }
   
   export default EditCommentModal;
