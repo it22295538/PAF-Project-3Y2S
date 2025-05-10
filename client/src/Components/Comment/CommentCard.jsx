7d5b40f32258461c5e3002baf2dd1b7d1eef9f7d
IT22267122 <it22267122@my.sliit.lk>
2025-05-10 20:13:16 +0530

    Update CommentCard.jsx

--- client/src/Components/Comment/CommentCard.jsx
+++ client/src/Components/Comment/CommentCard.jsx
@@ -62,8 +62,70 @@ const CommentCard = ({ comment }) => {
     }
     
   };
+  return (
+    <div>
+      <div className="reqUser flex justify-between items-center py-5">
+        <div className="flex items-center">
+          <div className="">
+            <img
+              className="w-9 h-9 rounded-full"
+              src={
+                comment?.userDto.userImage ||
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              }
+              alt=""
+            />
+          </div>
+          <div className="ml-3">
+            <p>
+              <span className="font-semibold"> {comment.userDto.username}</span>
+              <span className="ml-2">{comment.content}</span>
+            </p>
+            <div className="flex items-center space-x-3 text-xs opacity-60 pt-2">
+              <span>{timeDifference(comment?.createdAt)}</span>
+              {commentLikes > 0 && <span>{commentLikes} like</span>}
+              {user?.reqUser?.id === comment?.userDto?.id && (
+                <>
+                  <BsPencil
+                    className="cursor-pointer"
+                    onClick={handleClickOnEditComment}
+                  />
+                  <MdDelete
+                    className="cursor-pointer "
+                    onClick={handleDeleteComment}
+                  />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+        {isCommentLiked ? (
+          <AiFillHeart
+            onClick={handleUnLikeComment}
+            className="text-xs hover:opacity-50 cursor-pointer text-red-600"
+          />
+        ) : (
+          <AiOutlineHeart
+            onClick={handleLikeComment}
+            className="text-xs hover:opacity-50 cursor-pointer "
+          />
+        )}
+      </div>
+      {isEditCommentInputOpen && (
+        <div>
+          <input
+            className=" outline-none border-b border-black text-sm"
+            placeholder="Add Comment..."
+            type="text"
+            onKeyPress={handleEditComment}
+            onChange={handleCommnetInputChange}
+            value={commentContent}
+          />
+        </div>
+      )}
+    </div>
+  );
 
-  
   };
   
   export default CommentCard;
