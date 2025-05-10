b831a9724a125258b91ce553b0bee88292ebd280
IT22267122 <it22267122@my.sliit.lk>
2025-05-10 20:47:48 +0530

    Update Reducer.js

--- client/src/Redux/Comment/Reducer.js
+++ client/src/Redux/Comment/Reducer.js
@@ -8,3 +8,25 @@ const initialState={
     deletedComment:null,
     comments:null,
 }
+
+export const commentReducer=(store=initialState,{type,payload})=>{
+    if(type===CREATE_COMMENT){
+        return {...store, createdComment:payload}
+    }
+    else if(type===GET_POST_COMMENT){
+        return {...store, postComments:payload}
+    }
+    else if(type===LIKE_COMMENT){
+        return {...store, likedComment:payload}
+    }
+    else if(type===EDIT_COMMENT){
+        return {...store, updatedComment:payload}
+    }
+    else if(type===DELETE_COMMENT){
+        return{...store,deletedComment:payload}
+    }
+    else if(type===GET_ALL_COMMENT){
+        return{...store,comments:payload}
+    }
+    return store;
+}
