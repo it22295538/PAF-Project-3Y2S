3822cf7e3c5aa8756e0e8a5878fbeea59128e083
IT22267122 <it22267122@my.sliit.lk>
2025-05-10 20:27:56 +0530

    Update CommentModal.jsx

--- client/src/Components/Comment/CommentModal.jsx
+++ client/src/Components/Comment/CommentModal.jsx
@@ -86,6 +86,110 @@ import {
         navigate("/");
       };
     
+
+      return (
+        <div>
+          <Modal size={"4xl"} onClose={handleClose} isOpen={isOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalBody>
+                <div className="flex h-[75vh] ">
+                  <div className="w-[45%] flex flex-col justify-center">
+                    <img
+                      className="max-h-full max-w-full"
+                      src={post.singlePost?.image}
+                      alt=""
+                    />
+                  </div>
+                  <div className="w-[55%] pl-10 relative">
+                    <div className="reqUser flex justify-between items-center py-5">
+                      <div className="flex items-center">
+                        <div className="">
+                          <img
+                            className="w-9 h-9 rounded-full"
+                            src={
+                              user.reqUser?.image ||
+                              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                            }
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-3">
+                          <p>{post?.singlePost?.user?.name}</p>
+                          <p>{post?.singlePost?.user?.username}</p>
+                        </div>
+                      </div>
+                      <BsThreeDots/>
+                    </div>
+                    <hr />
+    
+                    <div className="comments ">
+                      {comments.comments?.length > 0 &&
+                        comments.comments?.map((item) => (
+                          <CommentCard comment={item} />
+                        ))}
+                    </div>
+    
+                    <div className=" absolute bottom-0 w-[90%]">
+                      <div className="flex justify-between items-center w-full mt-5">
+                        <div className="flex items-center space-x-2 ">
+                          {isPostLiked ? (
+                            <AiFillHeart
+                              onClick={handleUnLikePost}
+                              className="text-2xl hover:opacity-50 cursor-pointer text-red-600"
+                            />
+                          ) : (
+                            <AiOutlineHeart
+                              onClick={handleLikePost}
+                              className="text-2xl hover:opacity-50 cursor-pointer "
+                            />
+                          )}
+    
+                          <FaRegComment className="text-xl hover:opacity-50 cursor-pointer" />
+                          <RiSendPlaneLine className="text-xl hover:opacity-50 cursor-pointer" />
+                        </div>
+                        <div className="cursor-pointer">
+                          {isSaved ? (
+                            <BsBookmarkFill
+                              onClick={() => handleUnSavePost(post.singlePost?.id)}
+                              className="text-xl"
+                            />
+                          ) : (
+                            <BsBookmark
+                              onClick={() => handleSavePost(post.singlePost?.id)}
+                              className="text-xl hover:opacity-50 cursor-pointer"
+                            />
+                          )}
+                        </div>
+                      </div>
+                      {post.singlePost?.likedByUsers?.length > 0 && (
+                        <p className="text-sm font-semibold py-2">
+                          {post.singlePost?.likedByUsers?.length} likes{" "}
+                        </p>
+                      )}
+                      <p className="opacity-70 pb-5">
+                        {timeDifference(post?.singlePost?.createdAt)}
+                      </p>
+                      <div className=" flex items-center ">
+                        <BsEmojiSmile className="mr-3 text-xl" />
+                        <input
+                          className="commentInput w-[70%]"
+                          placeholder="Add Comment..."
+                          type="text"
+                          onKeyPress={handleOnEnterPress}
+                          onChange={handleCommnetInputChange}
+                          value={commentContent}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </div>
+      );
+
 };
 
 export default CommentModal;
