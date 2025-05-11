b8713e37f86311bff47794b8313dbdc467ab7ca3
LimethK <limethkurukulasooriya@gmail.com>
2025-05-11 07:12:23 +0530

    navigation storycircle

--- client/src/Components/Story/StoryCircle/StoryCircle.jsx
+++ client/src/Components/Story/StoryCircle/StoryCircle.jsx
@@ -3,6 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { findStoryByUserId } from "../../../Redux/Story/Action";
 
 
+const StoryCircle = ({ image, username, userId }) => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`story/${userId}`);
+  };
 
+  
+};
 
 export default StoryCircle;
