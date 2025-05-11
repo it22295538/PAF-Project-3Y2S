3c89427307a6a92c57ade2065a1e91830e3c4fce
LimethK <limethkurukulasooriya@gmail.com>
2025-05-11 07:03:17 +0530

    state createstory
    setting up state

--- client/src/Components/Story/CreateStory.jsx
+++ client/src/Components/Story/CreateStory.jsx
@@ -5,6 +5,15 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import { uploadToCloudinary } from "../../Config/UploadToCloudinary";
 
+const CreateStory = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+  const [caption, setCaption] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
+ 
 
 export default CreateStory; 
