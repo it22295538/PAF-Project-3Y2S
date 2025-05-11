191e7e130c5fd54f5f30e9ca2574a74595666238
Yash0dara <y.k.athapaththu@gmail.com>
2025-05-11 09:33:06 +0530

    Update PostCard.jsx
    updated post card.

--- client/src/Components/Post/PostCard/PostCard.jsx
+++ client/src/Components/Post/PostCard/PostCard.jsx
@@ -1,13 +1,30 @@
 import { useDisclosure } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { BsBookmark, BsBookmarkFill, BsDot, BsThreeDots } from "react-icons/bs";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { createNotificationAction } from "../../../Redux/Notification/Action"; 
+import {
+  BsBookmark,
+  BsBookmarkFill,
+  BsDot,
+  BsEmojiSmile,
+  BsThreeDots,
+} from "react-icons/bs";
+import { FaRegComment } from "react-icons/fa";
 import { RiSendPlaneLine } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { isReqUserPost, isSavedPost, timeDifference } from "../../../Config/Logic";
+import {
+  isPostLikedByUser,
+  isReqUserPost,
+  isSavedPost,
+  timeDifference,
+} from "../../../Config/Logic";
+import { createComment } from "../../../Redux/Comment/Action";
 import {
   deletePostAction,
+  likePostAction,
   savePostAction,
+  unLikePostAction,
   unSavePostAction,
 } from "../../../Redux/Post/Action";
 import CommentModal from "../../Comment/CommentModal";
@@ -16,20 +33,84 @@ import EditPostModal from "../Create/EditPostModal";
 import { IconButton } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
-const PostCard = ({ userProfileImage, username, location, post, createdAt }) => {
+const PostCard = ({
+  userProfileImage,
+  username,
+  location,
+  post,
+  createdAt,
+}) => {
+  const [commentContent, setCommentContent] = useState();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const { user } = useSelector((store) => store);
   const [isSaved, setIsSaved] = useState(false);
+  const [isPostLiked, setIsPostLiked] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [openEditPostModal, setOpenEditPostModal] = useState(false);
 
+  const handleCommentInputChange = (e) => {
+    setCommentContent(e.target.value);
+  };
+
+  const [numberOfLikes, setNumberOfLike] = useState(0);
+
   const data = {
     jwt: token,
     postId: post.id,
+  };
+
+  const handleAddComment = () => {
+    const data = {
+      jwt: token,
+      postId: post.id,
+      data: {
+        content: commentContent,
+      },
+    };
+    dispatch(createComment(data));
+    setCommentContent("");
+    
+    // Create notification for comment
+    if (post.user.id !== user.reqUser.id) { // Only create notification if not own post
+      const notification = {
+        message: `${user.reqUser.username} commented: ${commentContent}`,
+        type: "COMMENT",
+        postId: post.id
+      };
+      dispatch(createNotificationAction(notification, token));
+    }
+  };
+
+  const handleOnEnterPress = (e) => {
+    if (e.key === "Enter") {
+      handleAddComment();
+    }
+  };
+
+  const handleLikePost = () => {
+    dispatch(likePostAction(data));
+    setIsPostLiked(true);
+    setNumberOfLike(numberOfLikes + 1);
+    
+  
+    if (post.user.id !== user.reqUser.id) { 
+      const notification = {
+        message: `${user.reqUser.username} liked your post`,
+        type: "LIKE",
+        postId: post.id
+      };
+      dispatch(createNotificationAction(notification, token));
+    }
+  };
+
+  const handleUnLikePost = () => {
+    dispatch(unLikePostAction(data));
+    setIsPostLiked(false);
+    setNumberOfLike(numberOfLikes - 1);
   };
 
   const handleSavePost = () => {
@@ -47,11 +128,15 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
   };
 
   const handleNextMedia = () => {
-    setCurrentMediaIndex(prev => prev === post.mediaUrls.length - 1 ? 0 : prev + 1);
+    setCurrentMediaIndex(prev => 
+      prev === post.mediaUrls.length - 1 ? 0 : prev + 1
+    );
   };
 
   const handlePrevMedia = () => {
-    setCurrentMediaIndex(prev => prev === 0 ? post.mediaUrls.length - 1 : prev - 1);
+    setCurrentMediaIndex(prev => 
+      prev === 0 ? post.mediaUrls.length - 1 : prev - 1
+    );
   };
 
   const isVideo = (url) => {
@@ -60,6 +145,8 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
 
   useEffect(() => {
     setIsSaved(isSavedPost(user.reqUser, post.id));
+    setIsPostLiked(isPostLikedByUser(post, user.reqUser?.id));
+    setNumberOfLike(post?.likedByUsers?.length);
   }, [user.reqUser, post]);
 
   const handleClick = () => {
@@ -160,6 +247,7 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
           </div>
         </div>
 
+        {/* Media Slider Section */}
         <div className="w-full relative">
           {post.mediaUrls?.map((url, index) => (
             <div
@@ -184,6 +272,7 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
 
           {post.mediaUrls?.length > 1 && (
             <>
+              {}
               <div className="absolute bottom-2 left-0 right-0 flex justify-center space-x-2">
                 {post.mediaUrls?.map((_, index) => (
                   <button
@@ -202,6 +291,21 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
 
         <div className="flex justify-between items-center w-full px-5 py-4">
           <div className="flex items-center space-x-2">
+            {isPostLiked ? (
+              <AiFillHeart
+                onClick={handleUnLikePost}
+                className="text-2xl hover:opacity-50 cursor-pointer text-red-600"
+              />
+            ) : (
+              <AiOutlineHeart
+                onClick={handleLikePost}
+                className="text-2xl hover:opacity-50 cursor-pointer"
+              />
+            )}
+            <FaRegComment
+              onClick={handleOpenCommentModal}
+              className="text-xl hover:opacity-50 cursor-pointer"
+            />
             <RiSendPlaneLine className="text-xl hover:opacity-50 cursor-pointer" />
           </div>
           <div className="cursor-pointer">
@@ -219,9 +323,33 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
           </div>
         </div>
         <div className="w-full py-2 px-5">
+          {numberOfLikes > 0 && (
+            <p className="text-sm">{numberOfLikes} likes</p>
+          )}
           <p className="py-2">
             <span className="font-semibold">{post?.user?.username}</span> {post.caption}
           </p>
+          {post?.comments?.length > 0 && (
+            <p
+              onClick={handleOpenCommentModal}
+              className="opacity-50 text-sm py-2 -z-0 cursor-pointer"
+            >
+              View all {post?.comments?.length} comments
+            </p>
+          )}
+        </div>
+        <div className="border border-t w-full">
+          <div className="w-full flex items-center px-5">
+            <BsEmojiSmile />
+            <input
+              onKeyPress={handleOnEnterPress}
+              onChange={handleCommentInputChange}
+              value={commentContent}
+              className="commentInput"
+              type="text"
+              placeholder="Add a comment..."
+            />
+          </div>
         </div>
       </div>
 
@@ -233,6 +361,11 @@ const PostCard = ({ userProfileImage, username, location, post, createdAt }) => 
       />
 
       <CommentModal
+        handleLikePost={handleLikePost}
+        handleSavePost={handleSavePost}
+        handleUnSavePost={handleUnSavePost}
+        handleUnLikePost={handleUnLikePost}
+        isPostLiked={isPostLiked}
         isSaved={isSaved}
         postData={post}
         isOpen={isOpen}
